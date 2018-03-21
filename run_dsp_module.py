260b4cbbf5ccffded47301951244931dc56cb1f7
Greg-R <soapy-smith@moose.com>
2018-03-21 19:48:08 -0400

    Clean up.

--- run_dsp_module.py
+++ run_dsp_module.py
@@ -6,33 +6,36 @@ Created on Thu Mar 15 19:36:07 2018
 @author: raven
 """
 
+#  Import the DSP module which contains C functions.
+
 import dsp_module
 
 args = ["sines", "./recdigitalfiltersprogs/sines3.def", "2000", "1024"]
 argsEncoded = [x.encode('UTF-8') for x in args]
 
-from wurlitzer import sys_pipes
+#  The Wurlitzer allows stdio to be piped into Ipython.
+from wurlitzer import sys_pipes, pipes
 with sys_pipes():
     dsp_module.sine_gen(4, argsEncoded)
-    
-from wurlitzer import pipes
 
+#  This context manager pushes stdio to the variable out.
+#  This makes a giant string with \n delimiters.
+    
 with pipes() as (out, err):
     dsp_module.sine_gen(4, argsEncoded)
 
 #  stdout is a giant string.
 stdout = out.read()
 
-#  Transform stdout into something that matplotlib can digest.
-
-import matplotlib.pyplot as plt
-
-import numpy as np
-
+#import matplotlib.pyplot as plt
+#import numpy as np
 #plt.plot(stdout)
+
+#  Transform stdout into something that matplotlib can digest.
 
 waveformAlpha = stdout.split('\n')
 
+#  Create an array to dump the data into.
 waveformNumeric = []
 
 for x in waveformAlpha:
@@ -40,15 +43,8 @@ for x in waveformAlpha:
     if(x != ''):
 #        print(float(x))
         waveformNumeric.append(float(x))
-        
 
-#  Create Numpy array:
-        
-waveformArray = np.array(waveformNumeric)
-
-#plt.style.use('sineplotstyle')
-#plt.plot(waveformArray)
-#plt.xlim(0, 150.0)
+#  The intent is to use a purely object oriented approach to plotting.
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -64,15 +60,16 @@ fig = Figure()
 # argument.
 FigureCanvas(fig)
 ax = fig.add_subplot(111)
-#ax.plot([1, 2, 3])
 ax.set_title('Waveform')
 ax.grid(True, which='both')
 ax.set_xlabel('time')
 ax.set_ylabel('volts')
-ax.plot(waveformArray)
+ax.plot(waveformNumeric)
 ax.set_xlim(0,200)
 ax.minorticks_on()
 display(fig)
+
+#  Save the figure to a PDF file.
 
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -82,6 +79,10 @@ with PdfPages('foo.pdf') as pdf:
  #    pdf.savefig(fig)
      # When no figure is specified the current figure is saved
      pdf.savefig(fig)
+
+#  This configuration directory can hold a matplotlibrc
+#  file.  Although it didn't work with the Anaconda installation.
+#  This command left here for future reference.
      
-import matplotlib as plt     
-print(plt.get_configdir())
+#import matplotlib as plt     
+#print(plt.get_configdir())
